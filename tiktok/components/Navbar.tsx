baeb0707647705448da7ca7b41497633d2032016
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-07 22:20:56 +0100

    added my logo image

--- tiktok/components/Navbar.tsx
+++ tiktok/components/Navbar.tsx
@@ -12,9 +12,9 @@ import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
 <Link href="/">
-<div>
+<div className="w-[100px]">
 <Image
 className="cursor-pointer"
 src={Logo} alt="tiktik"
