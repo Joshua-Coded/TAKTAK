855db429b1e1d2831893d3f9ef90f2bc44bd29cd
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-07 12:37:18 +0100

    imported all my external dependencies

--- tiktok/components/Navbar.tsx
+++ tiktok/components/Navbar.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GoogleLogin, GoogleLogout  } from "react-google-login";
+import { AiOutlineLogout } from "react-icons/ai";
+import {BiSearch} from "react-icons/bi";
+import {IoMdAdd} from "react-icons/io";
+
+
+
 
 const Navbar = () => {
     return (
