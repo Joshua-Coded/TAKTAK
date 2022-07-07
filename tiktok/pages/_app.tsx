24eb865f1301cadbc62c53256040eaad667d459c
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-07 12:30:51 +0100

    added some sidebar setup

--- tiktok/pages/_app.tsx
+++ tiktok/pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {useState, useEffect} from "react";
 
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+import '../styles/globals.css'
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setisSSR] = useState(true);
 
@@ -14,10 +17,10 @@ setisSSR(false)
 
   return ( 
   <div>
-Navbar
+<Navbar />
 <div className="flex gap-6 md:gap-20">
   <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-   Sidebar
+   <Sidebar />
   </div>
 <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] video flex-1">
 <Component {...pageProps} />
