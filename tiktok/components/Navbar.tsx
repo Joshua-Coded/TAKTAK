b27531bbe5e7e7451232eba98cc47601b2ada7d4
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-07 22:23:45 +0100

    first part of navbar done

--- tiktok/components/Navbar.tsx
+++ tiktok/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
 <Link href="/">
-<div className="w-[100px]">
+<div className="w-[100px] md:w-[130px] md:h-[30px] h-[38px]">
 <Image
 className="cursor-pointer"
 src={Logo} alt="tiktik"
