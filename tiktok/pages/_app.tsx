3d58be14699ad439a51e6f0836766695ca366708
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-07 12:19:44 +0100

    index.ts setup

--- tiktok/pages/_app.tsx
+++ tiktok/pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useState, useEffect} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setisSSR] = useState(true);
+
+
+  useEffect(() => {
+setisSSR(false)
+  }, []);
+  
+  if(isSSR) return null;
+
+  return ( 
+  <div>
+Navbar
+<div className="flex gap-6 md:gap-20">
+  <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+   Sidebar
+  </div>
+<div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] video flex-1">
+<Component {...pageProps} />
+</div>
+</div>
+  </div>
+  );
 }
 
 export default MyApp
