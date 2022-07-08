5d4c812c28d2aa94f965df89caeb4e9cb7efdf64
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-08 15:45:25 +0100

    Google login setup

--- tiktok/components/Sidebar.tsx
+++ tiktok/components/Sidebar.tsx
@@ -7,13 +7,58 @@ import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import {ImCancelCircle} from "react-icons/im"; 
 
 const Sidebar = () => {
-    const [showSidebar, setshowSidebar] = useState(true);
     
+    const [showSidebar, setshowSidebar] = useState(true);
+ 
+    const userProfile = false;
+
+const normalLink = "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded";
+
+
     return (
         <div>
-<div className="block xl:hidden m-2 ml-4 mt-3 text-xl" onClick={() => setshowSidebar((prev) => !prev)}>
+<div className="
+block xl:hidden m-2 ml-4 
+mt-3 text-xl" 
+onClick={() => setshowSidebar((prev) => !prev)}>
 {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
 </div>
+
+{showSidebar && (
+    <div className="xl:w-400 w-20 
+    flex flex-col justify-start 
+    mb-10 border-r-2 border-gray-100
+    xl:border-0 p-3">
+    
+    <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+<Link href="/">
+    <div className={normalLink}>
+        <p className="text-2xl">
+<AiFillHome />
+        </p>
+        <span className="text-xl hidden xl:block">For you </span>
+    </div>
+</Link>
+</div>
+{!userProfile && (
+    <div className="px-2 py-4 hidden xl:block"> 
+        <p className="text-gray-400">
+        Login to like and comment on videos
+        </p>
+        <div className="pr-4">
+            <GoogleLogin 
+            clientId=""
+            onSuccess={() => {}}
+            onFailure={() => {}}
+            cookiePolicy="single_host_origin"
+            />
+            </div>
+    </div>
+)}
+</div>
+
+)}
+
         </div>
     )
 }
