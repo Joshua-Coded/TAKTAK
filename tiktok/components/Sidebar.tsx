5e6e3e84a70755f843d2b35fa51b0d74ad0e0cf0
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-08 15:24:25 +0100

    refactoring sidebar

--- tiktok/components/Sidebar.tsx
+++ tiktok/components/Sidebar.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import {NextPage} from "next";
+import {useRouter} from "next/router";
+import Link from "next/link";
+import GoogleLogin from "react-google-login";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import {ImCancelCircle} from "react-icons/im"; 
 
 const Sidebar = () => {
+    const [showSidebar, setshowSidebar] = useState(true);
+    
     return (
         <div>
-
+<div className="block xl:hidden m-2 ml-4 mt-3 text-xl" onClick={() => setshowSidebar((prev) => !prev)}>
+{showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+</div>
         </div>
     )
 }
