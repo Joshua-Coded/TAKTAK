6ccb4bd6d90d37833feda604c74e1bb77da26193
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-08 16:36:17 +0100

    added some script to the footer list file

--- tiktok/components/Footer.tsx
+++ tiktok/components/Footer.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+const List = ({items} : {items: string[]}) => (
+    <div className="flex flex-wrap gap-2 mt-5">
+            {footerList1.map((item) => (
+                <p key={item}>
+                    {item}
+                </p>
+            ))}
+        </div>
+)
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="mt-6 hidden xl:block">
+        <List items={footerList1}/>
+        <List items={footerList2}/>
+        <List items={footerList3}/>
+    </div>
   )
 }
 
