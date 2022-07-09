bcb2f9e0a4fb3656c89d62d28d2d2768092ab6c0
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-09 08:12:15 +0100

    created another script

--- tiktok/components/VideoCard.tsx
+++ tiktok/components/VideoCard.tsx
@@ -15,7 +15,25 @@ post: Video;
 
 const VideoCard: NextPage<IProps> = ({post}) => {
   return (
-    <div>VideoCard</div>
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+        <div>
+            <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+               <div className="md:16 md:h-16 w-10 h-10">
+                 <Link href="/">
+                     <>
+                      <Image  
+                      width={62}
+                      height={62}
+                      className="rounded-full"
+                      src={post.postedBy.image}
+                      alt="profile photo"
+                      layout="responsive"/>
+                      </>
+                 </Link>
+               </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
