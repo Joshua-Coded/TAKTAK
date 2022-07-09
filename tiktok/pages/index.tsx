abf2abe54294b6e8e96ad4daed1f254133c63232
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-09 07:49:45 +0100

    refactoring

--- tiktok/pages/index.tsx
+++ tiktok/pages/index.tsx
@@ -1,17 +1,44 @@
 import type { NextPage } from 'next'
+import axios from 'axios';
+import {Video} from "../types";
 import Head from 'next/head'
 import Image from 'next/image'
+import VideoCard from '../components/VideoCard';
+import NoResult from '../components/NoResult';
+
+interface IProps {
+  videos: Video[] 
+}
 
 
-const Home: NextPage = () => {
+const Home = ({videos}: IProps) => {
+console.log(videos);
   return (
-
-      
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+       <div className="flex flex-col gap-10 videos h-full">
+        {videos.length ? (
+          videos.map((video: Video) =>(
+            <VideoCard  post={video} key={video._id}/>
+          ))
+        ) : (
+          <NoResult text={'No Videos'} />
+        )}
+       </div>
           
   )
+}
+
+//fetching data in nextjs
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+
+return {
+  props: {
+    videos: data
+  }
+}
+
+
 }
 
 export default Home
--- tiktok/utils/client.ts
+++ tiktok/utils/client.ts
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'jrissguo',
   dataset: 'production',
-  apiVersion: '2022-03-10',
+  apiVersion: '2022-06-10',
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
