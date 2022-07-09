5094f1b913ebb62ea244595b86dc3e6c617278c1
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-09 07:48:55 +0100

    completed the post files

--- tiktok/pages/api/post/index.ts
+++ tiktok/pages/api/post/index.ts
@@ -1,13 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { info } from 'console'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { client } from '../../../utils/client';
+import { allPostsQuery } from '../../../utils/queries';
 
-type Data = {
-  name: string
+
+export default  async function handler(req: NextApiRequest,
+  res: NextApiResponse) {
+      if(req.method === 'GET') {
+    const query = allPostsQuery();
+
+const  data = await client.fetch(query);
+
+res.status(200).json(data);
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'Response Success' })
 }
