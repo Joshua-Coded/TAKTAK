693629297a8e2763cd47007e7fbe065588d6a984
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-09 07:58:29 +0100

    importing all icons

--- tiktok/components/VideoCard.tsx
+++ tiktok/components/VideoCard.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { Video } from '../types';
 import {NextPage} from "next";
+import Image from "next/image";
+import Link from "next/link";
+import {HiVolumeUp, HiVolumeOff} from "react-icons/hi";
+import {BsPlay,BsFillPlayFill, BsFillPauseFill} from "react-icons/bs";
+
+import { GoVerified } from "react-icons/go";
+
 
 interface IProps {
 post: Video;
