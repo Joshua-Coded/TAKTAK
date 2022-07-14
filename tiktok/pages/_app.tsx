de3e2ace879c6d9110067c3ddf9dbe7916bf9464
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-14 18:43:01 +0100

    refactored _app

--- tiktok/pages/_app.tsx
+++ tiktok/pages/_app.tsx
@@ -1,33 +1,40 @@
 import type { AppProps } from 'next/app'
+
 import {useState, useEffect} from "react";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 import '../styles/globals.css'
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+
   const [isSSR, setisSSR] = useState(true);
 
 
   useEffect(() => {
-setisSSR(false)
+
+    setisSSR(false)
+  
   }, []);
   
   if(isSSR) return null;
 
   return ( 
   <div>
-<Navbar />
-<div className="flex gap-6 md:gap-20">
-  <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-   <Sidebar />
+    <Navbar />
+       <div className="flex gap-6 md:gap-20">
+         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+    <Sidebar />
   </div>
-<div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] video flex-1">
-<Component {...pageProps} />
-</div>
-</div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+              <Component {...pageProps} />
+          </div>
   </div>
-  );
+</div>
+
+);
+
 }
 
 export default MyApp
