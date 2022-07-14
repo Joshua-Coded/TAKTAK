e0b9bcab386a8b008545368b0be878bbbd5383aa
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-14 18:39:50 +0100

    retouched the video card

--- tiktok/components/VideoCard.tsx
+++ tiktok/components/VideoCard.tsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef} from 'react'
 import { Video } from '../types';
-import {NextPage} from "next";
+import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import {HiVolumeUp, HiVolumeOff} from "react-icons/hi";
-import {BsPlay,BsFillPlayFill, BsFillPauseFill} from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsPlay,BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 
 import { GoVerified } from "react-icons/go";
 
@@ -13,10 +13,24 @@ interface IProps {
 post: Video;
 }
 
-const VideoCard: NextPage<IProps> = ({post}) => {
- const  [isHover, setIsHover] = useState(false)
+      const VideoCard: NextPage<IProps> = ({post}) => {
+    const  [isHover, setIsHover] = useState(false)
+    const [playing, setplaying] = useState(false)
+    const [isVideoMuted, setIsVideoMuted] = useState(false)
+  
+    const videoRef = useRef<HTMLVideoElement>(null);
 
-    return (
+const onVideoPress = () => {
+    if(playing) {
+        videoRef?.current?.pause();
+        setplaying(false);
+    } else {
+        videoRef?.current?.play();
+        setplaying(true);
+    }
+}
+
+return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
         <div>
             <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
@@ -41,38 +55,59 @@ const VideoCard: NextPage<IProps> = ({post}) => {
                         font-bold text-primary">
                             {post.postedBy.userName} {`
                             `}
+                            <GoVerified 
+                                className="text-blue-500
+                                text-md" />
                             </p>
                             <p className="capitalize font-medium text-xs
                             text-gray-500 hidden md:block">
                                 {post.postedBy.userName}
                                 </p>
-                        <GoVerified 
-                        className="text-blue-500
-                         text-md" />
                     </div>
                 </Link>
                </div>
             </div>
         </div>
 
-<div className="lg:ml-20 flex gap-4 relative">
-    <div 
-    onMouseEnter={() => setIsHover(true)}
-    onMouseLeave={() => setIsHover(false)} 
-    className="rounded-3xl">
-        <Link href="/">
+        <div className="lg:ml-20 flex gap-4 relative">
+            <div 
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)} 
+            className="rounded-3xl">
+            <Link href="/">
             <video
             loop
-            src={post.video.asset.url}
+            ref={videoRef}
             className="lg:w[600px] h-[300px] md:h-[400px]
             lg:h-[530px] w-[200] rounded-2xl cursor-pointer bg-gray-100"
+            src={post.video.asset.url}
             >
-                
             </video>
         </Link>
         {isHover && (
-            <div>
+            <div className="absolute bottom-6
+            cursor-pointer left-8 md:left-14 lg:left-0
+            flex gap-10 lg:justify-between w-[100px] md:w-[50px]
+            p-3">
+                   {playing ? (
+                    <button onClick={ onVideoPress}>
+                        <BsFillPauseFill className="text-black text-2xl lg:text-4xl " />
+                    </button>
+                   ) : (
+                    <button onClick={ onVideoPress}>
+                        <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                    </button>
+                   )}
 
+                   {isVideoMuted ? (
+                    <button onClick={() => setIsVideoMuted(false)}>
+                        <HiVolumeOff className="text-black text-2xl lg:text-4xl " />
+                    </button>
+                   ) : (
+                    <button onClick={() => setIsVideoMuted(true)}>
+                        <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                    </button>
+                   )}          
             </div>
         )}
     </div>
