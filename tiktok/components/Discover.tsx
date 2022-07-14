f6bc9cbaed8d82dfd59cdde1ea044814b344ac8f
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-14 18:41:49 +0100

    retouched the discover page

--- tiktok/components/Discover.tsx
+++ tiktok/components/Discover.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 
-import {topics} from "../utils/constants";
+import { topics } from "../utils/constants";
 
 const Discover = () => {
 
 const router = useRouter();
 const {topic} = router.query;
 
-const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#f51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#f51997]";
+const activeTopicStyle = 
+"xl:border-2 hover:bg-primary xl:border-[#f51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#f51997]";
 
-const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+const topicStyle =
+ "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
 
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
@@ -21,7 +23,7 @@ const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 ro
         </p>
         <div className="flex gap-3 flex-wrap">
             {topics.map((item) => (
-                <Link href={`/?topic = ${item.name}`} key={item.name}>
+                <Link href={`/?topic=${item.name}`} key={item.name}>
                     <div className={topic === item.name ? activeTopicStyle : topicStyle}>
                         <span className="font-bold text-2xl xl-text-md">
                             {item.icon}
