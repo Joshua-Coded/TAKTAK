420daa75a4f47ccbe3b48c4c8dd6cfba151ed05c
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-14 18:40:31 +0100

    retouched the footer page

--- tiktok/components/Footer.tsx
+++ tiktok/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { footerList1, footerList2, footerList3 } from "../utils/constants";
 
-const List = ({items, mt} : {items: string[], mt:boolean}) => (
+const List = ({ items, mt} : { items: string[], mt:boolean}) => (
     <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
             {footerList1.map((item) => (
                 <p key={item}>
@@ -17,7 +17,7 @@ const Footer = () => {
         <List items={footerList1} mt={false}/>
         <List items={footerList2} mt/>
         <List items={footerList3} mt/>
-        <p className="text-gray-400 text-sm mt-5">2022  joshdev TikTak</p>
+        <p className="text-gray-400 text-sm mt-5">2022 @Dev-Josh TikTak</p>
     </div>
   )
 }
