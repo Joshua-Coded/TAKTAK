3fe6932c8cf8047646185fb8ff30e6d62db55644
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-14 18:43:55 +0100

    refactored

--- tiktok/pages/api/post/index.ts
+++ tiktok/pages/api/post/index.ts
@@ -1,17 +1,19 @@
-import { info } from 'console'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { client } from '../../../utils/client';
 import { allPostsQuery } from '../../../utils/queries';
 
+type Data = {
+  name: string
+}
 
 export default  async function handler(req: NextApiRequest,
   res: NextApiResponse) {
-      if(req.method === 'GET') {
+
+    if(req.method === 'GET') {
     const query = allPostsQuery();
+    const  data = await client.fetch(query);
 
-const  data = await client.fetch(query);
-
-res.status(200).json(data);
+      res.status(200).json(data);
 }
 
-}
+  }
